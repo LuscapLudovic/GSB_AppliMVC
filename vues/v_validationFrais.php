09724a4378a569cdda8d1d1003463c7b620c5e12
Ludo <kiluhasenpai@gmail.com>
2019-01-05 15:54:54 +0100

    For Ponyland !!

--- vues/v_validationFrais.php
+++ vues/v_validationFrais.php
@@ -5,6 +5,10 @@
  * Date: 12/12/2018
  * Time: 14:57
  */
-?>
 
-// on fera la page de validation de frais ici conformément à l'image qu'il y a dans le dossier sujet.
+require 'vues/v_enteteGestion.php';
+?>
+// écrivez le code ici !!
+<?php
+require 'vues/v_pied.php';
+?>
